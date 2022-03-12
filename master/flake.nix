153cd29bf2e10ad5f2a87a4ea5da8f74c4147bb6
hugosenari <hugosenari@gmail.com>
2022-03-12 03:23:29 -0300

    chore: re index meta

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''View and set the metadata for audio files'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-meta-master.flake = false;
+  inputs.src-meta-master.owner = "RainbowAsteroids";
+  inputs.src-meta-master.ref   = "refs/heads/master";
+  inputs.src-meta-master.repo  = "meta";
+  inputs.src-meta-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-meta-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-meta-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
